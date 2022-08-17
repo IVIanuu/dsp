907c4784140374747823cdc90ef31d2da40e91e5
manu <IVIanuu@gmail.com>
2022-08-17 11:05:46 +0200

    Added vertical equalizer

--- app/src/main/kotlin/com/ivianuu/dsp/HomeUi.kt
+++ app/src/main/kotlin/com/ivianuu/dsp/HomeUi.kt
@@ -4,13 +4,36 @@
 
 package com.ivianuu.dsp
 
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
 import com.ivianuu.essentials.data.DataStore
 import com.ivianuu.essentials.state.action
 import com.ivianuu.essentials.state.bind
+import com.ivianuu.essentials.ui.common.HorizontalList
 import com.ivianuu.essentials.ui.common.VerticalList
 import com.ivianuu.essentials.ui.material.Scaffold
+import com.ivianuu.essentials.ui.material.Slider
 import com.ivianuu.essentials.ui.material.Subheader
 import com.ivianuu.essentials.ui.material.TopAppBar
 import com.ivianuu.essentials.ui.material.incrementingStepPolicy
@@ -21,6 +44,7 @@ import com.ivianuu.essentials.ui.prefs.ScaledPercentageUnitText
 import com.ivianuu.essentials.ui.prefs.SliderListItem
 import com.ivianuu.essentials.ui.prefs.SwitchListItem
 import com.ivianuu.injekt.Provide
+import kotlin.math.absoluteValue
 
 @Provide object HomeKey : RootKey
 
@@ -39,17 +63,10 @@ import com.ivianuu.injekt.Provide
         Subheader { Text("Equalizer") }
       }
 
-      items(
-        eq.toList()
+      item {
+        Equalizer(eq = eq.toList()
           .sortedBy { it.first }
-      ) { (band, value) ->
-        SliderListItem(
-          value = value,
-          onValueChange = { updateEqBand(band, it) },
-          title = { Text(band.toString()) },
-          stepPolicy = incrementingStepPolicy(0.05f),
-          valueText = { ScaledPercentageUnitText(it) }
-        )
+          .toMap(), onBandChange = updateEqBand)
       }
 
       item {
@@ -64,6 +81,99 @@ import com.ivianuu.injekt.Provide
           stepPolicy = incrementingStepPolicy(0.05f),
           valueText = { ScaledPercentageUnitText(it) }
         )
+      }
+    }
+  }
+}
+
+@Composable fun Equalizer(
+  eq: Map<Float, Float>,
+  onBandChange: (Float, Float) -> Unit
+) {
+  HorizontalList(
+    contentPadding = PaddingValues(start = 16.dp, end = 16.dp),
+    horizontalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+    eq.forEach { (band, value) ->
+      item(key = band) {
+        Column(
+          modifier = Modifier
+            .padding(top = 24.dp, bottom = 16.dp)
+            .width(36.dp),
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.Center
+        ) {
+          Text(
+            text = when {
+              band < 1000 -> band.toInt().toString()
+              band < 10000 -> {
+                if (band.toInt().toString().drop(1).all { it == '0' })
+                  band.toInt().toString()[0] + "k"
+                else
+                  band.toInt().toString()[0] + "." + band.toInt().toString()[1] + "k"
+              }
+              else -> {
+                if (band.toInt().toString().drop(2).all { it == '0' })
+                  band.toInt().toString().take(2) + "k"
+                else
+                  band.toInt().toString().take(2) + "." + band.toInt().toString()[2] + "k"
+              }
+            },
+            style = MaterialTheme.typography.caption
+          )
+
+          Spacer(Modifier.height(8.dp))
+
+          var internalValue by remember(value) { mutableStateOf(value) }
+
+          Layout(
+            modifier = Modifier
+              .height(200.dp)
+              .fillMaxWidth(),
+            content = {
+              Slider(
+                modifier = Modifier
+                  .rotate(-90f),
+                value = internalValue,
+                onValueChange = { internalValue = it },
+                onValueChangeFinished = { onBandChange(band, internalValue) },
+                stepPolicy = incrementingStepPolicy(0.05f)
+              )
+            }
+          ) { measurables, constraints ->
+            val placeable = measurables.single()
+              .measure(Constraints.fixed(constraints.maxHeight, constraints.maxWidth))
+
+            layout(constraints.maxWidth, constraints.maxHeight) {
+              placeable.place(
+                constraints.maxWidth / 2 - placeable.width / 2,
+                constraints.maxHeight / 2 - placeable.height / 2
+              )
+            }
+          }
+
+          Spacer(Modifier.height(8.dp))
+
+          CompositionLocalProvider(
+            LocalTextStyle provides MaterialTheme.typography.caption
+          ) {
+            val valueRange = 0f..1f
+            val steps = incrementingStepPolicy(0.05f)(valueRange)
+            val stepFractions = (if (steps == 0) emptyList()
+            else List(steps + 2) { it.toFloat() / (steps + 1) })
+            val stepValues = stepFractions
+              .map {
+                valueRange.start +
+                    ((valueRange.endInclusive - valueRange.start) * it)
+              }
+
+            val steppedValue = stepValues
+              .minByOrNull { (it - internalValue).absoluteValue }
+              ?: internalValue
+
+            ScaledPercentageUnitText(steppedValue)
+          }
+        }
       }
     }
   }
