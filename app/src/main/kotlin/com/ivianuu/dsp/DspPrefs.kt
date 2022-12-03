4190fae2343241c5624b83e6bfcd8ed609d506f3
manu <IVIanuu@gmail.com>
2022-12-03 09:29:47 +0100

    ...

--- app/src/main/kotlin/com/ivianuu/dsp/DspPrefs.kt
+++ app/src/main/kotlin/com/ivianuu/dsp/DspPrefs.kt
@@ -24,14 +24,24 @@ import kotlinx.serialization.Serializable
   val postGain: Float = 0.0f
 )
 
-const val EQ_DB = 10f
-const val BASS_BOOST_DB = 10f
-const val POST_GAIN_DB = 40f
+const val EQ_DB = 15f
+const val BASS_BOOST_DB = 15f
+const val POST_GAIN_DB = 100f
 
 val EqBands = listOf(
+  25f,
+  40f,
   60f,
-  230f,
-  910f,
-  3600f,
-  140000f
+  100f,
+  160f,
+  250f,
+  400f,
+  630f,
+  1000f,
+  1600f,
+  2500f,
+  4000f,
+  6300f,
+  10000f,
+  16000f
 )
